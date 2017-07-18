27f609e69b9105c1f0f05c8285d1ca23593cd972
Ruandre <ruandrer@bidvestdata.co.za>
2017-07-18 09:45:24 +0200

    Add initial files

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const argv = require('yargs')
+  .alias('i', 'imgPath')
+  .argv
+
+const fs = require('fs-extra')
+const path = require('path')
+const image = path.resolve(__dirname, argv.imgPath)
+
+const Tesseract = require('tesseract.js')
+Tesseract.recognize(image)
+  .progress(info => {
+    console.log(info)
+  })
+  .then(data => {
+    console.log(data.text)
+    fs.writeFileSync('out.txt', data.text)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+  .finally(e => {
+    console.log('Done!')
+    process.exit()
+  })
